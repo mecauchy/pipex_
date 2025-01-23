aa19a0b146dd2afcd5f2be694e4461b9f696731a
Melissa Cauchy <melissacauchy@MacBook-Air-de-Melissa.local>
2025-01-23 14:35:32 +0100

    add_utils

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcauchy- <mcauchy-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mecauchy <mecauchy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 13:43:13 by mcauchy-          #+#    #+#             */
-/*   Updated: 2025/01/22 13:45:08 by mcauchy-         ###   ########.fr       */
+/*   Updated: 2025/01/23 13:52:47 by mecauchy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,53 +14,82 @@
 
 void    first_cmd_exec(int *fd, char **av,char **env)
 {
-        int infile;
+        int     infile;
+        char    **cmd_args;
+        char    *cmd_path;
         
         infile = open(av[1], O_RDONLY);
         if (infile < 0)
-            ft_perror("open infile error");
+            perror("open infile error");
+        cmd_args = ft_split(av[2], ' ');
+        if (!cmd_args)
+            perror("cmd1 split error");
+        cmd_path = get_command_path(av[2], env);
+        if (!cmd_path)
+        {
+            free(cmd_args);
+            free(cmd_path);
+            perror("cmd1 get_command_path error");
+            exit(127);
+        }
         dup2(infile, STDIN_FILENO);
         dup2(fd[1], STDOUT_FILENO);
         close(fd[0]);
         close(fd[1]);
         close(infile);
-        execve(get_command_path(av[2], env), parse_args(av[2]), env);
-        ft_perror("execve cmd1 error");
+        execve(cmd_path, cmd_args, env);
+        free(cmd_args);
+        perror("execve cmd1 error");
 }
 
 void    second_cmd_exec(int *fd, char **av, char **env)
 {
-        int outfile;
+        int     outfile;
+        char    **cmd_args;
+        char    *cmd_path;
         
         outfile = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
         if (outfile < 0)
-            ft_perror("open outfile error");
+            perror("open outfile error");
+        cmd_args = ft_split(av[3], ' ');
+        if (!cmd_args)
+            perror("cmd2 split error");
+        cmd_path = get_command_path(av[3], env);
+        if (!cmd_path)
+        {
+            free(cmd_args);
+            free(cmd_path);
+            perror("cmd1 get_command_path error");
+            exit(127);
+        }
         dup2(fd[0], STDIN_FILENO);
         dup2(outfile, STDOUT_FILENO);
         close(fd[0]);
         close(fd[1]);
         close(outfile);
-        execve(get_command_path(av[3], env), parse_args(av[3]), env);
-        ft_perror("execve cmd2 error");
+        execve(cmd_path, cmd_args, env);
+        free(cmd_args);
+        perror("execve cmd2 error");
 }
 
 void pipex(int ac, char **av, char **env)
 {
     int fd[2];
-    int pid1, pid2;
+    int pid1;
+    int pid2;
 
     if (ac < 5)
-        ft_error("error : arguments");
+        ft_putendl_fd("error : arguments", 2);
     if (pipe(fd) == -1)
-        ft_perror("pipe error");
+        perror("pipe error");
     pid1 = fork();
     if (pid1 == -1)
-        ft_perror("[PID1] ERROR");
+        perror("[PID1] ERROR");
     if (pid1 == 0)
         first_cmd_exec(fd, av, env);
     pid2 = fork();
     if (pid2 == -1)
-        ft_perror("[PID2] ERROR");
+        perror("[PID2] ERROR");
     if (pid2 == 0)
         second_cmd_exec(fd, av, env);
     close(fd[0]);
@@ -71,9 +100,8 @@ void pipex(int ac, char **av, char **env)
 
 int main(int ac, char **av, char **env)
 {
-   // char *envp[] = {"PATH=/usr/bin", NULL};
     if (ac == 1)
-        ft_error("error : arguments");
-    pipex(ac, av, envp);
+        ft_putendl_fd("error : arguments", 2);
+    pipex(ac, av, env);
     return (0);
 }
--- srcs/utils.c
+++ srcs/utils.c
@@ -3,20 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcauchy- <mcauchy-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mecauchy <mecauchy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 13:43:18 by mcauchy-          #+#    #+#             */
-/*   Updated: 2025/01/22 16:30:23 by mcauchy-         ###   ########.fr       */
+/*   Updated: 2025/01/23 14:01:57 by mecauchy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-void	get_command_path(char *cmd, char **env)
+char	*ft_is_path(char *path, char *cmd)
+{
+	char	*try;
+	int		i;
+	int		j;
+
+	i = 0;
+	try = malloc(sizeof(char *) * (ft_strlen(path) + ft_strlen(cmd) + 2));
+	if (!try)
+		return (NULL);
+	while (path[i])
+	{
+		try[j] = path[i];
+		printf("try[%d]: %c\n", j, try[j]);
+		i++;
+		j++;
+	}
+	//j++;
+	try[j] = '/';
+	printf("try[%d]: %c\n", j, try[j]);
+	i = 0;
+	while (cmd[i])
+	{
+		try[j] = cmd[i];
+		printf("try[%d]: %c\n", j, try[j]);
+		i++;
+		j++;
+	}
+	try[j] = '\0';
+	return (try);
+}
+char	*get_command_path(char *cmd, char **env)
 {
 	int		i;
 	char	*get_path;
 	char	*full_path;
+	char	*add_slash;
+	char	**cut_cmd;
 	char	**path;
 	i = 0;
 	while (env[i])
@@ -29,20 +62,74 @@ void	get_command_path(char *cmd, char **env)
 		i++;
 	}
 	if (!get_path)
-		ft_error();
-	path = ft_split(get_path, ":");
+		ft_putendl_fd("Error: PATH not found", 2);
+	path = ft_split(get_path, ':');
+	if (!path)
+		return (NULL);
+	cut_cmd = ft_split(cmd, ' ');
+	if (!cut_cmd)
+		return (NULL);
 	i = 0;
 	while (path[i])
 	{
-		full_path = malloc(sizeof(char *) * (ft_strlen(path) + ft_strlen(cmd) + 2));
-		if (!full_path)
-			ft_error();
-		if (access((full_path[i]), X_OK) == 0)
+		add_slash = ft_strjoin(path[i], "/");
+		if (!add_slash)
+			ft_putendl_fd("malloc error", 2);
+		full_path = ft_strjoin(add_slash, cut_cmd[0]);
+		free(add_slash);
+		// printf("path[%d]: %s\n", i, path[i]);
+		// printf("*************full_path: %s\n", full_path);
+		// if (!full_path)
+		// 	ft_putendl_fd("malloc error", 2);
+		if (access((full_path), X_OK) == 0)
 		{
+			printf("full_path: %s\n", full_path);
 			free(path);
 			return (full_path);
 		}
 		free(full_path);
 		i++;
 	}
+	free(path);
+	return (NULL);
 }
+
+// char	*get_command_path(char *cmd, char **env)
+// {
+// 	int		i;
+// 	char	*get_path;
+// 	char	*full_path;
+// 	char	**path;
+// 	i = 0;
+// 	while (env[i])
+// 	{
+// 		if (ft_strncmp(env[i], "PATH=", 5) == 0)
+// 		{
+// 			get_path = env[i] + 5;
+// 			break ;
+// 		}
+// 		i++;
+// 	}
+// 	if (!get_path)
+// 		ft_putendl_fd("Error: PATH not found", 2);
+// 	path = ft_split(get_path, ':');
+// 	i = 0;
+// 	while (path[i])
+// 	{
+// 		full_path = ft_is_path(path[i], cmd);
+// 		printf("path[%d]: %s\n", i, path[i]);
+// 		printf("*************full_path: %s\n", full_path);
+// 		if (!full_path)
+// 			ft_putendl_fd("malloc error", 2);
+// 		if (access((full_path), X_OK) == 0)
+// 		{
+// 			printf("full_path: %s\n", full_path);
+// 			free(path);
+// 			return (full_path);
+// 		}
+// 		free(full_path);
+// 		i++;
+// 	}
+// 	free(path);
+// 	return (NULL);
+// }
